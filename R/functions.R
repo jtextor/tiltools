5bce70c3a841b08fb254f7083d6296fbdad5e298
Johannes Textor <johannes.textor@gmx.de>
2017-11-24 00:09:22 +0100

    new viz

--- R/functions.R
+++ R/functions.R
@@ -25,7 +25,12 @@ NULL
 #' @param cex point character expansion to use for individual points.
 #' @param color.levels how many different colors to use for heatmap.
 #' @param ... further arguments to be passed to the plotting function,
-#'  such as \code{xlim} or \code{xlab}. 
+#'  such as \code{xlim} or \code{xlab}.
+#' @examples
+#' densityplot2d( facsdata[,1], facsdata[,2], tf.fun=sqrt, nbin=100 )
+#' pc <- princomp(facsdata)
+#' densityplot2d( pc$scores[,1], pc$scores[,2], tf.fun=sqrt, nbin=100 )
+#' densityplot2d( pc$scores[,1], pc$scores[,2], z=facsdata[,2],tf.fun=sqrt, nbin=100, colfunc=colorRampPalette(c("black","red")) )
 #' @export
 densityplot2d <- function( x, y, z=NULL, nbins=50, max.points=5, 
 	colfunc=colorRampPalette(c("gray","darkblue","lightblue", "green", "yellow","orange", "red")), tf.fun=identity, pch=19, cex=0.3, color.levels=20,
@@ -65,68 +70,53 @@ densityplot2d <- function( x, y, z=NULL, nbins=50, max.points=5,
 		z.matrix <- matrix( z.matrix, nrow=nbins, byrow=TRUE )
 	}
 
-	if( is.null( z ) ){
-        	plot( x[points.not.in.image], y[points.not.in.image],
-			pch=pch, cex=cex, col=colfunc(color.levels)[1],
-			xlim=range(x.bin), 
-			ylim=range(y.bin), type='p' )
+        plot( NA,
+		xlim=range(x.bin), 
+		ylim=range(y.bin), ... )
 
-	        image( head(x.bin,-1)+diff(x.bin)/2, head(y.bin,-1)+diff(y.bin)/2, 
-			tf.fun(density.matrix),
-			useRaster=FALSE, add=TRUE,
-			col=colfunc(color.levels), ... )
-	} else {
-	}
+		M <- tf.fun( density.matrix )
+		zlim <- range(M[is.finite(M)])
 
+ 		M <- (M - zlim[1L])/diff(zlim)
+		col <- colfunc( color.levels )
 
-        #x.points <- c(0,1,2,5,10,20,50,100,200,500,1000)
-        #axis( 1, log( 1+x.points, base=2 ), as.character(x.points) )
-        #axis( 2, log( 1+x.points, base=2 ), as.character(x.points) )
+		if( !is.null(z) ){
+			zp <- z[points.not.in.image]
+			zlim <- range( z.matrix[is.finite(z.matrix)] )
+			z.matrix <- 1-(z.matrix-zlim[1L])/diff(zlim)
+        		zi <- floor((color.levels - 1e-05) * z.matrix + 1e-07)
+			zi[zi < 0 | zi >= color.levels] <- NA
+			zp <- 1-(zp-zlim[1L])/diff(zlim)
+			zp <- floor((color.levels - 1e-05) * zp + 1e-07)
+			zp[zp < 0 | zp >= color.levels] <- NA
+		}
 
-}
+        	Mi <- floor((color.levels - 1e-05) * M + 1e-07)
+		Mi[Mi < 0 | Mi >= color.levels] <- NA
 
+		if( is.null(z) ){
+			#1D lookup
+			Mc <- col[Mi+1L]
+		} else {
+			#2D lookup
+			col <- sapply(rev(col),function(x)
+				colorRampPalette(c("white",x))(color.levels+1) )
+			col <- col[2:(color.levels+1),,drop=FALSE]
+			Mc <- col[cbind(c(Mi+1L),c(zi+1L))]
+		}
 
-newplot2d <- function( x, y, nbins=50, max.points=5, 
-	colfunc=colorRampPalette(c("gray","darkblue","lightblue", "green", "yellow","orange", "red")), tf.fun=identity, pch=19, cex=0.3, color.levels=20,
-	... ){
-	x <- as.numeric(x)
-	y <- as.numeric(y)
-	if( any( is.na(x) ) ){
-                stop("NA values in X!")
-        }
-        if( any( is.na(y) ) ){
-		stop("NA values in Y!")
-	}
-        x.bin <- seq(floor(min(x)-.001), ceiling(max(x)+.001), length=nbins)
-	y.bin <- seq(floor(min(y)-.001), ceiling(max(y)+.001), length=nbins)
+		dim(Mc) <- dim(M)
 
-	x.int <- as.integer(findInterval( x, x.bin ))
-        y.int <- as.integer(findInterval( y, y.bin ))
+		rasterImage( t(Mc)[nrow(Mc):1, ,drop=FALSE],
+			min(x.bin), min(y.bin), max(x.bin), max(y.bin), interpolate=FALSE )
+		if( is.null( z ) ){
+	        	points( x[points.not.in.image], y[points.not.in.image],
+				pch=pch, cex=cex, col=colfunc(color.levels)[1] )
+		} else {
+	        	points( x[points.not.in.image], y[points.not.in.image],
+				pch=pch, cex=cex, col=col[1,zp+1] )
+		}
 
-        pix.ry <- length(y.bin) 
-
-        pix <- x.int*pix.ry + y.int
-        ptbl <- tabulate( pix )
-
-        my.freq.x <- floor((1:length(ptbl))/pix.ry)
-        my.freq.y <- (1:length(ptbl)) %% pix.ry
-
-        my.freq <- cbind(my.freq.x, my.freq.y, ptbl )
-        my.freq <- my.freq[my.freq.x > 0 & my.freq.y > 0,]
-
-        freq2D <- diag(nbins-1)*0
-        freq2D[cbind(my.freq[,1], my.freq[,2])] <- my.freq[,3]
-        f.int <- freq2D[cbind(x.int,y.int)]	
-	freq2D[freq2D <= max.points] <- NA
-
-	freq2D <- tf.fun(freq2D)
-
-        image( head(x.bin,-1)+diff(x.bin)/2, head(y.bin,-1)+diff(y.bin)/2, freq2D,
-		useRaster=FALSE,
-		col=colfunc(color.levels), ... )
-
-        points( x[f.int<=max.points], y[f.int<=max.points],
-		pch=pch, cex=cex, col=colfunc(color.levels)[1] )
 
         #x.points <- c(0,1,2,5,10,20,50,100,200,500,1000)
         #axis( 1, log( 1+x.points, base=2 ), as.character(x.points) )
